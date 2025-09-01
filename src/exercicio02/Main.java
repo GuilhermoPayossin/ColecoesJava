31521059eb4db852f335fc66598af8c2bcbcd4a3
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-01 20:48:18 -0300

    Exercicios 1 e 2

---
+++ src/exercicio02/Main.java
@@ -0,0 +1,16 @@
+package exercicio02;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Aluno> turmaBD = new HashSet<>();
+        turmaBD.add(new Aluno(10, "A", "A"));
+        turmaBD.add(new Aluno(20, "B", "B"));
+        turmaBD.add(new Aluno(10, "C", "C"));
+        turmaBD.add(new Aluno(30, "D", "D"));
+
+        turmaBD.forEach(aluno -> System.out.println(aluno));
+    }
+}
